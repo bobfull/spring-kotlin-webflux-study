4afb00bd05e3fb294fc546738aaf0e5eeca0d761
bob-full <bob.full@kakaocorp.com>
2019-03-14 13:13:08 +0900

    Router추가, Hello world

---
+++ src/main/kotlin/com/microservice/study/router/CustomerRouter.kt
@@ -0,0 +1,24 @@
+package com.microservice.study.router
+
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.body
+import org.springframework.web.reactive.function.server.router
+import reactor.core.publisher.toMono
+
+@Component
+class CustomerRouter {
+
+    @Bean
+    fun customerRoutes(): RouterFunction<*> = router {
+        "/functional".nest {
+            "/customer".nest{
+                GET("/") {
+                    ServerResponse.ok().body("hello world".toMono(), String::class.java)
+                }
+            }
+        }
+    }
+}
