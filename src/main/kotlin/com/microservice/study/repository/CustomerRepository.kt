81707dbd256fa54e1ad242091787976c94d2547e
bob-full <bob.full@kakaocorp.com>
2019-03-18 08:50:11 +0900

    Reactive Mongo Template 사용

--- src/main/kotlin/com/microservice/study/repository/CustomerRepository.kt
+++ src/main/kotlin/com/microservice/study/repository/CustomerRepository.kt
@@ -1,6 +1,21 @@
 package com.microservice.study.repository
 
 import com.microservice.study.data.Customer
-import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.stereotype.Repository
+import reactor.core.publisher.Mono
+import reactor.core.publisher.toMono
+import javax.annotation.PostConstruct
 
-interface CustomerRepository : ReactiveCrudRepository<Customer, Int>
+@Repository
+class CustomerRepository(private val template: ReactiveMongoTemplate) {
+    companion object {
+        val initialCustomer = listOf(Customer(1, "Kotiln"), Customer(2, "Spring"),
+                Customer(3, "microservices", Customer.Telephone("+44", "12345")))
+    }
+
+    @PostConstruct
+    fun initializeRepository() = initialCustomer.map(Customer::toMono).map(this::create).map(Mono<Customer>::subscribe)
+
+    fun create(customer: Mono<Customer>) = template.save(customer)
+}
